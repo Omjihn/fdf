22404a28c2558679713cba805de1d9eed294c8e3
Gregoire Bricot <gbricot@c2r5p7.42perpignan.fr>
2023-06-28 17:14:55 +0200

    lol nimp

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: gbricot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:43:22 by gbricot           #+#    #+#             */
-/*   Updated: 2023/06/28 15:41:37 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/06/28 17:14:18 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <limits.h>
 
 static int	ft_close_button(t_vars *vars)
 {
@@ -26,17 +27,49 @@ static int	ft_wich_key(int keycode, t_vars *vars)
 	return (0);
 }
 
-static int	ft_mouse(int keycode, t_vars *vars)
+void	ft_draw_lines(t_vars *vars, int x, int y, int color)
 {
-	int	x = 100;
-	int	y = 100;
-	//(void) vars;
-	if (keycode == 1)
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+
+	i = x;
+	j = x;
+	k = y;
+	l = y;
+	while (i > 500 && j < 0 && l < 0 && k > 500)
 	{
-        	mlx_mouse_get_pos(vars->mlx, vars->win, &x, &y);
-        	//mlx_pixel_put(vars->mlx, vars->win, x, y, 100);
-		printf("X :%d - Y :%d\n", x, y);
+		if (i > 500)
+		{
+			mlx_pixel_put(vars->mlx, vars->win, x, i, color);
+			i++;
+		}
+		if (j < 0)
+		{
+			mlx_pixel_put(vars->mlx, vars->win, x, j, color);
+			j--;
+		}
+		if (k > 500)
+                {
+                        mlx_pixel_put(vars->mlx, vars->win, k, y, color);
+                        k++;
+                }
+		if (l < 0)
+                {
+                        mlx_pixel_put(vars->mlx, vars->win, l, y, color);
+                        l--;
+                }
 	}
+}
+
+
+static int	ft_mouse(int keycode, int x, int y, t_vars *vars)
+{
+	printf("mouse_x %d, mouse_y %d keycode %d\n", vars->mouse_x, vars->mouse_y, keycode);
+	//mlx_pixel_put(vars->mlx, vars->win, x, y, rand());
+	ft_draw_lines(vars, x, y, rand());
+	//printf("X :%d - Y :%d\n", x, y);
         return (0);
 }
 
@@ -49,12 +82,15 @@ int	main(int ac, char **av)
 	vars = ft_init_fdf(av);
 	if (!vars)
 		return (0);
+	vars->mouse_x = 250;
+	vars->mouse_y = 250;
 	vars->mlx = mlx_init();
 	vars->win = mlx_new_window(vars->mlx, 500, 500, "bite.lolmdr");
 	vars->img = mlx_new_image(vars->mlx, 500, 500);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
 	mlx_hook(vars->win, 17, 0L, ft_close_button, vars);
 	mlx_key_hook(vars->win, ft_wich_key, vars);
+	mlx_pixel_put(vars->mlx, vars->win, vars->mouse_x, vars->mouse_y, 255);
 	mlx_mouse_hook(vars->win, ft_mouse, vars);
 /*	int	i = 0;
 	int	j = 0;
@@ -70,5 +106,5 @@ int	main(int ac, char **av)
 		printf ("\n");
 	}*/
 	mlx_loop(vars->mlx);
-	ft_free_all(vars);
+	return (0);
 }
