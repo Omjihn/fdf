50f232d3be70392d262c9ed5f83ed8f104da76d0
Gregoire Bricot <gbricot@c1r1p5.42perpignan.fr>
2023-07-06 20:48:49 +0200

    fixed

--- src/ft_draw_algos.c
+++ src/ft_draw_algos.c
@@ -6,7 +6,7 @@
 /*   By: gbricot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 18:27:28 by gbricot           #+#    #+#             */
-/*   Updated: 2023/07/06 19:07:24 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/07/06 20:44:32 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_draw_octant_0(int x1, int y1, int x2, int y2, t_vars *vars)
 {
-	int		color;
         float   error;
         float   slope;
 
@@ -22,16 +21,11 @@ void	ft_draw_octant_0(int x1, int y1, int x2, int y2, t_vars *vars)
 	x2 *= vars->zoom;
 	y1 *= vars->zoom;
 	y2 *= vars->zoom;
-
-	color = vars->map_info->map[x1][y1].color;
-	if (color == 0)
-		color = INT_MAX;
-
 	error = 0.0;
 	slope = (float) (y2 - y1) / (float) (x2 - x1);
 	while (x1 <= x2)
 	{
-		mlx_pixel_put(vars->mlx, vars->win, x1, y1, color);
+		mlx_pixel_put(vars->mlx, vars->win, x1, y1, INT_MAX);
 		error += slope;
 		if (error >= 0.5)
 		{
@@ -44,7 +38,6 @@ void	ft_draw_octant_0(int x1, int y1, int x2, int y2, t_vars *vars)
 
 void	ft_draw_octant_1(int x1, int y1, int x2, int y2, t_vars *vars)
 {
-	int		color;
 	float	error;
 	float	slope;
 
@@ -52,16 +45,11 @@ void	ft_draw_octant_1(int x1, int y1, int x2, int y2, t_vars *vars)
         x2 *= vars->zoom;
         y1 *= vars->zoom;
         y2 *= vars->zoom;
-
-	color = vars->map_info->map[x1][y1].color;
-        if (color == 0)
-                color = INT_MAX;
-
 	error = 0.0;
 	slope = (float) (x2 - x1) / (float) (y2 - y1);
 	while (y1 <= y2)
 	{
-		mlx_pixel_put(vars->mlx, vars->win, x1, y1, color);
+		mlx_pixel_put(vars->mlx, vars->win, x1, y1, INT_MAX);
 		error += slope;
 		if (error >= 0.5)
 		{
